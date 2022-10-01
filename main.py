8e1e530085a6922d6cd93e169bbc1a3cc0f922e1
Martin Caddy <26031043+martincaddy@users.noreply.github.com>
2022-10-01 16:51:17 +0000

    Choice of lists available

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import csv
 import random
 
 #import class list of names from a csv file
-
-with open('clss.csv', newline='') as f:
-    reader = csv.reader(f)
-    stc = list(reader)
-
 #user selection of number of names and run in loop
+swoop = int(input("Which list do you want to use 1 for full 2 for short "))
 
+if swoop == 1:
+  with open('clss.csv', newline='') as f:
+      reader = csv.reader(f)
+      stc = list(reader)
+else:
+  with open('clss2.csv', newline='') as f:
+      reader = csv.reader(f)
+      stc = list(reader)
+  
 chosen = int(input("How many names do you want to choose: "))
 #chosen = 1
 while chosen != 0 :
