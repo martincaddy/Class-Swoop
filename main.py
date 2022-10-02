2149bf78145d2e7c2730b68f1278a90c965a2e70
Martin Caddy <26031043+martincaddy@users.noreply.github.com>
2022-10-02 13:22:19 +0000

    Added an option to user define a list of names

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 
 #import class list of names from a csv file
 #user selection of number of names and run in loop swoop is the variable for class list choice
-swoop = int(input("Which list do you want to use \n1 Full Class \n2 Year 4 \n3 Year 5 \n4 Target \n \n"))
+swoop = int(input("Which list do you want to use \n1 Full Class \n2 Year 4 \n3 Year 5 \n4 Define your own list \n5 Target \n \n"))
 
 #user defined time delay
 delay = int(input("How many seconds do you want to wait for each name picked? "))
@@ -23,7 +23,18 @@ elif swoop == 3:
   with open('Year5.csv', newline='') as f:
       reader = csv.reader(f)
       stc = list(reader)
-    
+
+elif swoop == 4:
+# creating an empty list
+  stc = [] 
+# number of elements as input
+  n = int(input("How many names do you want to add? : ")) 
+# iterating till the range
+  for i in range(0, n):
+      ele = str(input('Name: ')) 
+      stc.append(ele) # adding the element
+      
+#print(lst)    
 else:
   with open('clss2.csv', newline='') as f:
       reader = csv.reader(f)
